32ccae8f1f2d2dd6f9602cd39ebc3e312128d8eb
PR0I <sarah.popelka1@cms.hhs.gov>
2025-07-01 08:51:14 -0400

    removing the nonsense secret value to make the security gods happy

--- src/app/settings.py
+++ src/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fklzzu1u7x+i%20=x+tjf-ds+b1%f0xk4m@%km74fcpf#k=tr('
+SECRET_KEY = os.get('NDH_DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
