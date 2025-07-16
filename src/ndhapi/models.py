f041cc81a235afaa344ad84eb08c277168a204d6
PR0I <sarah.popelka1@cms.hhs.gov>
2025-07-16 09:25:35 -0400

    Initial progress toward provider endpoint

--- src/ndhapi/models.py
+++ src/ndhapi/models.py
@@ -1,3 +1,385 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
+
+class Address(models.Model):
+    barcode_delivery_code = models.CharField(max_length=12, blank=True, null=True)
+    smarty_key = models.CharField(max_length=10, blank=True, null=True)
+    address_us_id = models.IntegerField(blank=True, null=True)
+    address_international_id = models.IntegerField(blank=True, null=True)
+    address_nonstandard_id = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'address'
+
+
+class AddressInternational(models.Model):
+    address_id = models.IntegerField()
+    input_id = models.CharField(max_length=36, blank=True, null=True)
+    country = models.CharField(max_length=64, blank=True, null=True)
+    geocode = models.CharField(max_length=4, blank=True, null=True)
+    local_language = models.CharField(max_length=6, blank=True, null=True)
+    freeform = models.CharField(max_length=512, blank=True, null=True)
+    address1 = models.CharField(max_length=64, blank=True, null=True)
+    address2 = models.CharField(max_length=64, blank=True, null=True)
+    address3 = models.CharField(max_length=64, blank=True, null=True)
+    address4 = models.CharField(max_length=64, blank=True, null=True)
+    organization = models.CharField(max_length=64, blank=True, null=True)
+    locality = models.CharField(max_length=64, blank=True, null=True)
+    administrative_area = models.CharField(max_length=32, blank=True, null=True)
+    postal_code = models.CharField(max_length=16, blank=True, null=True)
+    administrative_area_iso2 = models.CharField(max_length=8, blank=True, null=True)
+    sub_administrative_area = models.CharField(max_length=64, blank=True, null=True)
+    country_iso_3 = models.CharField(max_length=3, blank=True, null=True)
+    premise = models.CharField(max_length=64, blank=True, null=True)
+    premise_number = models.CharField(max_length=64, blank=True, null=True)
+    thoroughfare = models.CharField(max_length=64, blank=True, null=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
+    geocode_precision = models.CharField(max_length=32, blank=True, null=True)
+    max_geocode_precision = models.CharField(max_length=32, blank=True, null=True)
+    address_format = models.CharField(max_length=128, blank=True, null=True)
+    verification_status = models.CharField(max_length=32, blank=True, null=True)
+    address_precision = models.CharField(max_length=32, blank=True, null=True)
+    max_address_precision = models.CharField(max_length=32, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'address_international'
+
+
+class AddressNonstandard(models.Model):
+    address_id = models.IntegerField()
+    input_id = models.CharField(max_length=36, blank=True, null=True)
+    input_index = models.IntegerField(blank=True, null=True)
+    candidate_index = models.IntegerField(blank=True, null=True)
+    addressee = models.CharField(max_length=64, blank=True, null=True)
+    delivery_line_1 = models.CharField(max_length=64, blank=True, null=True)
+    delivery_line_2 = models.CharField(max_length=64, blank=True, null=True)
+    last_line = models.CharField(max_length=64, blank=True, null=True)
+    address_type = models.CharField(max_length=32, blank=True, null=True)
+    address_format = models.CharField(max_length=128, blank=True, null=True)
+    raw_address = models.TextField(blank=True, null=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
+    notes = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'address_nonstandard'
+
+
+class AddressUs(models.Model):
+    address_id = models.IntegerField()
+    input_id = models.CharField(max_length=36, blank=True, null=True)
+    input_index = models.IntegerField(blank=True, null=True)
+    candidate_index = models.IntegerField(blank=True, null=True)
+    addressee = models.CharField(max_length=64, blank=True, null=True)
+    delivery_line_1 = models.CharField(max_length=64, blank=True, null=True)
+    delivery_line_2 = models.CharField(max_length=64, blank=True, null=True)
+    last_line = models.CharField(max_length=64, blank=True, null=True)
+    delivery_point_barcode = models.CharField(max_length=12, blank=True, null=True)
+    urbanization = models.CharField(max_length=64, blank=True, null=True)
+    primary_number = models.CharField(max_length=30, blank=True, null=True)
+    street_name = models.CharField(max_length=64, blank=True, null=True)
+    street_predirection = models.CharField(max_length=16, blank=True, null=True)
+    street_postdirection = models.CharField(max_length=16, blank=True, null=True)
+    street_suffix = models.CharField(max_length=16, blank=True, null=True)
+    secondary_number = models.CharField(max_length=32, blank=True, null=True)
+    secondary_designator = models.CharField(max_length=16, blank=True, null=True)
+    extra_secondary_number = models.CharField(max_length=32, blank=True, null=True)
+    extra_secondary_designator = models.CharField(max_length=16, blank=True, null=True)
+    pmb_designator = models.CharField(max_length=16, blank=True, null=True)
+    pmb_number = models.CharField(max_length=16, blank=True, null=True)
+    city_name = models.CharField(max_length=64, blank=True, null=True)
+    default_city_name = models.CharField(max_length=64, blank=True, null=True)
+    state_code = models.CharField(max_length=2, blank=True, null=True)
+    zipcode = models.CharField(max_length=5, blank=True, null=True)
+    plus4_code = models.CharField(max_length=4, blank=True, null=True)
+    delivery_point = models.CharField(max_length=2, blank=True, null=True)
+    delivery_point_check_digit = models.CharField(max_length=1, blank=True, null=True)
+    record_type = models.CharField(max_length=1, blank=True, null=True)
+    zip_type = models.CharField(max_length=32, blank=True, null=True)
+    county_code = models.CharField(max_length=5, blank=True, null=True)
+    ews_match = models.CharField(max_length=5, blank=True, null=True)
+    carrier_route = models.CharField(max_length=4, blank=True, null=True)
+    congressional_district = models.CharField(max_length=2, blank=True, null=True)
+    building_default_indicator = models.CharField(max_length=1, blank=True, null=True)
+    rdi = models.CharField(max_length=12, blank=True, null=True)
+    elot_sequence = models.CharField(max_length=4, blank=True, null=True)
+    elot_sort = models.CharField(max_length=4, blank=True, null=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
+    coordinate_license = models.IntegerField(blank=True, null=True)
+    geo_precision = models.CharField(max_length=18, blank=True, null=True)
+    time_zone = models.CharField(max_length=48, blank=True, null=True)
+    utc_offset = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
+    dst = models.CharField(max_length=5, blank=True, null=True)
+    dpv_match_code = models.CharField(max_length=1, blank=True, null=True)
+    dpv_footnotes = models.CharField(max_length=32, blank=True, null=True)
+    dpv_cmra = models.CharField(max_length=1, blank=True, null=True)
+    dpv_vacant = models.CharField(max_length=1, blank=True, null=True)
+    dpv_no_stat = models.CharField(max_length=1, blank=True, null=True)
+    active = models.CharField(max_length=1, blank=True, null=True)
+    footnotes = models.CharField(max_length=24, blank=True, null=True)
+    lacslink_code = models.CharField(max_length=2, blank=True, null=True)
+    lacslink_indicator = models.CharField(max_length=1, blank=True, null=True)
+    suitelink_match = models.CharField(max_length=5, blank=True, null=True)
+    enhanced_match = models.CharField(max_length=64, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'address_us'
+
+
+class ClinicalCredential(models.Model):
+    acronym = models.CharField(max_length=20)
+    name = models.CharField(max_length=100, blank=True, null=True)
+    source_url = models.CharField(max_length=250, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'clinical_credential'
+
+
+class ClinicalSchool(models.Model):
+    name = models.CharField(max_length=20)
+    url = models.CharField(max_length=500, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'clinical_school'
+
+
+class FhirAddressType(models.Model):
+    value = models.TextField(unique=True)
+
+    class Meta:
+        managed = False
+        db_table = 'fhir_address_type'
+
+
+class FhirNameType(models.Model):
+    value = models.CharField(unique=True, max_length=50)
+
+    class Meta:
+        managed = False
+        db_table = 'fhir_name_type'
+
+
+class FipsCounty(models.Model):
+    id = models.CharField(primary_key=True, max_length=5)
+    name = models.CharField(unique=True, max_length=200)
+    fips_state_id = models.CharField(max_length=2)
+
+    class Meta:
+        managed = False
+        db_table = 'fips_county'
+
+
+class FipsState(models.Model):
+    id = models.CharField(primary_key=True, max_length=2)
+    name = models.CharField(unique=True, max_length=100)
+    abbreviation = models.CharField(unique=True, max_length=2)
+
+    class Meta:
+        managed = False
+        db_table = 'fips_state'
+
+
+class Individual(models.Model):
+    ssn = models.CharField(max_length=10, blank=True, null=True)
+    sex_code = models.CharField(max_length=1, blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
+    npi = models.ForeignKey('Npi', models.DO_NOTHING, db_column='npi', blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'individual'
+
+
+class IndividualToAddress(models.Model):
+    pk = models.CompositePrimaryKey('individual_id', 'address_id')
+    individual_id = models.IntegerField()
+    address_type_id = models.IntegerField()
+    address = models.ForeignKey(Address, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'individual_to_address'
+
+
+class IndividualToClinicalCredential(models.Model):
+    pk = models.CompositePrimaryKey('individual_id', 'clinical_credential_id')
+    individual_id = models.IntegerField()
+    clinical_credential_id = models.IntegerField()
+    receipt_date = models.DateField(blank=True, null=True)
+    clinical_school_id = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'individual_to_clinical_credential'
+
+
+class IndividualToEmailAddress(models.Model):
+    pk = models.CompositePrimaryKey('individual_id', 'email_address')
+    individual_id = models.IntegerField()
+    email_address = models.CharField(max_length=300)
+
+    class Meta:
+        managed = False
+        db_table = 'individual_to_email_address'
+
+
+class IndividualToLanguageSpoken(models.Model):
+    pk = models.CompositePrimaryKey('individual_id', 'language_spoken_id')
+    individual_id = models.IntegerField()
+    language_spoken_id = models.CharField(max_length=2)
+
+    class Meta:
+        managed = False
+        db_table = 'individual_to_language_spoken'
+
+
+class IndividualToName(models.Model):
+    pk = models.CompositePrimaryKey('individual_id', 'last_name', 'first_name', 'middle_name', 'name_prefix', 'name_suffix', 'fhir_name_type_id', 'effective_date')
+    individual = models.ForeignKey(Individual, models.DO_NOTHING)
+    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=21)
+    name_prefix = models.CharField(max_length=6)
+    name_suffix = models.CharField(max_length=6)
+    fhir_name_type_id = models.IntegerField()
+    effective_date = models.DateField()
+    end_date = models.DateField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'individual_to_name'
+
+
+class IndividualToNuccTaxonomyCode(models.Model):
+    pk = models.CompositePrimaryKey('individual_id', 'nucc_taxonomy_code_id', 'state_id')
+    individual_id = models.IntegerField()
+    nucc_taxonomy_code_id = models.IntegerField()
+    state_id = models.CharField(max_length=2)
+    license_number = models.CharField(max_length=20, blank=True, null=True)
+    is_primary = models.BooleanField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'individual_to_nucc_taxonomy_code'
+
+
+class IndividualToOtherIdentifier(models.Model):
+    pk = models.CompositePrimaryKey('individual_id', 'value', 'state_id')
+    individual_id = models.IntegerField()
+    value = models.CharField(max_length=21)
+    other_identifier_type_id = models.IntegerField()
+    state_id = models.CharField(max_length=2)
+    issuer_name = models.CharField(max_length=81, blank=True, null=True)
+    issue_date = models.DateField(blank=True, null=True)
+    expiry_date = models.DateField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'individual_to_other_identifier'
+
+
+class IndividualToPhoneNumber(models.Model):
+    pk = models.CompositePrimaryKey('individual_id', 'phone_number_id', 'phone_type_id')
+    individual_id = models.IntegerField()
+    phone_type_id = models.IntegerField()
+    phone_number_id = models.IntegerField()
+    extension = models.CharField(max_length=10, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'individual_to_phone_number'
+
+
+class LanguageSpoken(models.Model):
+    id = models.CharField(primary_key=True, max_length=2)
+    value = models.CharField(max_length=200, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'language_spoken'
+
+
+class MedicareProviderType(models.Model):
+    value = models.CharField(unique=True)
+
+    class Meta:
+        managed = False
+        db_table = 'medicare_provider_type'
+
+
+class Npi(models.Model):
+    npi = models.BigIntegerField(primary_key=True)
+    entity_type_code = models.SmallIntegerField()
+    replacement_npi = models.BigIntegerField(blank=True, null=True)
+    enumeration_date = models.DateField()
+    last_update_date = models.DateField()
+    deactivation_reason_code = models.CharField(max_length=3, blank=True, null=True)
+    deactivation_date = models.DateField(blank=True, null=True)
+    reactivation_date = models.DateField(blank=True, null=True)
+    certification_date = models.DateField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'npi'
+
+
+class NuccTaxonomyCode(models.Model):
+    id = models.CharField(primary_key=True, max_length=10)
+    display_name = models.TextField()
+    parent_id = models.CharField(max_length=10, blank=True, null=True)
+    definition = models.TextField(blank=True, null=True)
+    notes = models.TextField(blank=True, null=True)
+    certifying_board_name = models.TextField(blank=True, null=True)
+    certifying_board_url = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'nucc_taxonomy_code'
+
+
+class NuccTaxonomyCodeToMedicareProviderType(models.Model):
+    medicare_provider_type_id = models.IntegerField()
+    nucc_taxonomy_code_id = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'nucc_taxonomy_code_to_medicare_provider_type'
+
+
+class OtherIdentifierType(models.Model):
+    value = models.TextField(unique=True)
+
+    class Meta:
+        managed = False
+        db_table = 'other_identifier_type'
+
+
+class PhoneNumber(models.Model):
+    value = models.CharField(unique=True, max_length=20)
+
+    class Meta:
+        managed = False
+        db_table = 'phone_number'
+
+
+class PhoneType(models.Model):
+    value = models.TextField(unique=True)
+
+    class Meta:
+        managed = False
+        db_table = 'phone_type'
