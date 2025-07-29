a1a4c394a85816e8528a680c2b85f4861e681197
PR0I <sarah.popelka1@cms.hhs.gov>
2025-07-29 08:58:42 -0400

    Fixing some to_representation things

--- src/ndhfhir/serializers.py
+++ src/ndhfhir/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
 from fhir.resources.practitioner import Practitioner
 from fhir.resources.bundle import Bundle
-from .models import IndividualToEmailAddress, ProviderToOtherIdentifier, IndividualToName, Provider, Individual
-from fhir.resources.practitioner import Practitioner as FHIRPractitioner
+from .models import Npi, IndividualToEmailAddress, ProviderToOtherIdentifier, IndividualToName, Provider, Individual
+from fhir.resources.practitioner import Practitioner
 from fhir.resources.humanname import HumanName
 from fhir.resources.identifier import Identifier
 from fhir.resources.contactpoint import ContactPoint
@@ -10,26 +10,129 @@ from fhir.resources.codeableconcept import CodeableConcept
 from fhir.resources.coding import Coding
 from fhir.resources.period import Period
 from fhir.resources.meta import Meta
+from fhir.resources.address import Address
 from .cache import other_identifier_type, fhir_name_use
+import json
+
+
+class AddressSerializer(serializers.Serializer):
+    primary_number = serializers.CharField(
+        source='addressus__primary_number', read_only=True)
+    street_predirection = serializers.CharField(
+        source='addressus__street_predirection', read_only=True)
+    street_name = serializers.CharField(
+        source='addressus_street_name', read_only=True)
+    postdirection = serializers.CharField(
+        source='addressus__postdirection', read_only=True)
+    street_suffix = serializers.CharField(
+        source='addressus__street_suffix', read_only=True)
+    secondary_designator = serializers.CharField(
+        source='addressus__secondary_designator', read_only=True)
+    secondary_number = serializers.CharField(
+        source='addressus__secondary_number', read_only=True)
+    extra_secondary_designator = serializers.CharField(
+        source='addressus__extra_secondary_designator', read_only=True)
+    extra_secondary_number = serializers.CharField(
+        source='addressus__extra_secondary_number', read_only=True)
+    city_name = serializers.CharField(
+        source='addressus__city_name', read_only=True)
+    state_abbreviation = serializers.CharField(
+        source='addressus__fipsstate__abbrev', read_only=True)
+    zipcode = serializers.CharField(
+        source='addressus__zipcode', read_only=True)
+
+    class Meta:
+        fields = ['primary_number', 'street_predirection', 'street_name', 'postdirection', 'street_suffix',
+                  'secondary_designator', 'secondary_number', 'extra_secondary_designator', 'extra_secondary_number',
+                  'city_name', 'state_abbreviation', 'zipcode']
+
+    def to_representation(self, obj):
+        addressLine1 = f"{obj.primary_number} {obj.street_predirection} {obj.street_name} {obj.postdirection} {obj.street_suffix}"
+        addressLine2 = f"{obj.secondary_designator} {obj.secondary_number}"
+        addressLine3 = f"{obj.extra_secondary_designator} {obj.extra_secondary_number}"
+        cityStateZip = f"f{obj.city_name}, {obj.state_abbreviation} {obj.zipcode}"
+        address = Address(
+            line=[addressLine1, addressLine2, addressLine3, cityStateZip],
+            use=address.address_type.value
+        )
+        return address.model_dump()
+
 
 class EmailSerializer(serializers.Serializer):
-    email_address = serializers.CharField(read_only = True)
+    email_address = serializers.CharField(read_only=True)
 
     class Meta:
         fields = ['email_address']
 
+    def to_representation(self, obj):
+        email_contact = ContactPoint(
+            system="email",
+            value=obj.email_address,
+            # use="work" TODO: add email use
+        )
+        return email_contact.model_dump()
 
-class OtherIdentifierSerializer(serializers.Serializer):
-    value = serializers.CharField(read_only = True)
-    issue_date = serializers.DateField(read_only = True)
-    expiry_date = serializers.DateField(read_only = True)
+
+class PhoneSerializer(serializers.Serializer):
+    phone_number = serializers.CharField(
+        source='phonenumber__value', read_only=True)
+    system = serializers.CharField(
+        source='fhirphonesystem__value', read_only=True)
+    use = serializers.CharField(source='fhirphoneuse__value', read_only=True)
+    extension = serializers.CharField(read_only=True)
 
     class Meta:
-        fields = ['value', 'issue_date', 'expiry_date','other_identifier_type', 'other_identifier_type_id', 'other_identifier_type_value']
+        fields = ['phone_number', 'system', 'use', 'extension']
+
+    def to_representation(self, obj):
+        phone_contact = ContactPoint(
+            system=obj.system,
+            use=obj.use,
+            value=f"{obj.phone_number} ext. {obj.extension}"
+        )
+        return phone_contact.model_dump()
+
+
+"""class TaxonomySerializer(serializers.Serializer):
+    id = serializers.CharField(source='nucctaxonomycode__id', read_only=True)
+    display_name = serializers.CharField(
+        source='nucctaxonomycode__display_name', read_only=True)
+
+    class Meta:
+        fields = ['id', 'display_name']
+
+    def to_representation(self, obj):
+        code = CodeableConcept(
+            coding=[Coding(
+                    system="http://nucc.org/provider-taxonomy",
+                    code=obj.nucc_taxonomy_code.id,
+                    display=obj.nucc_taxonomy_code.display_name
+                    )]
+        )
+        qualification = {
+            'identifier': Identifier(
+                value="test",
+                type=code,  # TODO: Replace
+                period=Period()
+            ),
+            'code': code,
+        }
+        return qualification"""
+
+
+class OtherIdentifierSerializer(serializers.Serializer):
+    value = serializers.CharField(read_only=True)
+    issue_date = serializers.DateField(read_only=True)
+    expiry_date = serializers.DateField(read_only=True)
+
+    class Meta:
+        fields = ['value', 'issue_date', 'expiry_date', 'other_identifier_type',
+                  'other_identifier_type_id', 'other_identifier_type_value']
+
     def to_representation(self, id):
         other_identifier_type_id = id.other_identifier_type_id
         license_identifier = Identifier(
-            #system="", TODO: Figure out how to associate a system with each identifier
+            # system="", TODO: Figure out how to associate a system with each identifier
             value=id.value,
             type=CodeableConcept(
                 coding=[Coding(
@@ -38,54 +141,127 @@ class OtherIdentifierSerializer(serializers.Serializer):
                     display=other_identifier_type[other_identifier_type_id]
                 )]
             ),
-            #use="" TODO: Add use for other identifier
+            # use="" TODO: Add use for other identifier
             period=Period(
                 start=id.issue_date,
                 end=id.expiry_date
-        )
+            )
         )
         return license_identifier.model_dump()
 
+
 class NameSerializer(serializers.Serializer):
-    last_name = serializers.CharField(read_only = True)
-    first_name = serializers.CharField(read_only = True)
-    middle_name = serializers.CharField(read_only = True)
-    effective_date = serializers.DateField(read_only = True)
-    end_date = serializers.DateField(read_only = True)
-    prefix = serializers.CharField(read_only = True)
-    suffix = serializers.CharField(read_only = True)
+    last_name = serializers.CharField(read_only=True)
+    first_name = serializers.CharField(read_only=True)
+    middle_name = serializers.CharField(read_only=True)
+    effective_date = serializers.DateField(read_only=True)
+    end_date = serializers.DateField(read_only=True)
+    prefix = serializers.CharField(read_only=True)
+    suffix = serializers.CharField(read_only=True)
+
     class Meta:
-        fields = ['last_name', 'first_name', 'middle_name', 'effective_date', 'end_date','prefix', 'suffix']
+        fields = ['last_name', 'first_name', 'middle_name',
+                  'effective_date', 'end_date', 'prefix', 'suffix']
+
     def to_representation(self, name):
+        name_parts = [part for part in [name.prefix, name.first_name,
+                                        name.middle_name, name.last_name, name.suffix] if part != '' and part is not None]
         human_name = HumanName(
+            use=fhir_name_use[name.fhir_name_use_id],
+            text=' '.join(name_parts),
             family=name.last_name,
             given=[name.first_name, name.middle_name],
-            use=fhir_name_use[name.fhir_name_use_id],
+            prefix=[name.prefix],
+            suffix=[name.suffix],
             period=Period(
                 start=name.effective_date,
                 end=name.end_date
             )
         )
-        if name.prefix!='':
-            human_name.prefix = [name.prefix]
-        if name.suffix!='':
-            human_name.suffix = [name.suffix]
         return human_name.model_dump()
 
-class PractitionerSerializer(serializers.ModelSerializer):
-    npi = serializers.CharField(source="npi__npi", read_only=True)
-    name = NameSerializer(source = 'individual__individualtoname', read_only = True, many = True)
-    email = EmailSerializer(source = 'individual__individualtoemail', read_only = True, many = True)
-    identifier = OtherIdentifierSerializer(source='providertootheridentifier_set', many = True, read_only = True)
-   
+
+class NPISerializer(serializers.ModelSerializer):
     class Meta:
-        model = Provider
-        fields = ['npi', 'name', 'email', 'identifier']
+        model = Npi
+        fields = '__all__'
+
+
+class IndividualSerializer(serializers.Serializer):
+    name = NameSerializer(
+        source='individualtoname_set', read_only=True, many=True)
+    email = EmailSerializer(
+        source='individualtoemail_set', read_only=True, many=True)
+    phone = PhoneSerializer(
+        source='individualtophone_set', many=True, read_only=True)
+
+    class Meta:
+        fields = ['name', 'email', 'phone']
+
+    def to_representation(self, obj):
+        representation = super().to_representation(obj)
+        individual = {
+            'name': representation['name']
+        }
+        telecom = []
+        if 'phone' in representation.keys():
+            telecom.append(representation['phone'])
+        if 'email' in representation.keys():
+            telecom.append(representation['email'])
+        individual['telecom'] = telecom
+        return individual
+
+
+class PractitionerSerializer(serializers.Serializer):
+    npi = NPISerializer()
+    individual = IndividualSerializer(read_only=True)
+    identifier = OtherIdentifierSerializer(
+        source='providertootheridentifier_set', many=True, read_only=True)
+    # taxonomy = TaxonomySerializer(
+    #    source='providertonucctaxonomycode_set', many=True, read_only=True)
+
+    class Meta:
+        fields = ['npi', 'name', 'email', 'phone', 'identifier', 'taxonomy']
+
+    def to_representation(self, obj):
+        representation = super().to_representation(obj)
+        practitioner = Practitioner()
+        practitioner.id = str(obj.npi.npi)
+        practitioner.meta = Meta(
+            profile=[
+                "http://hl7.org/fhir/us/core/StructureDefinition/us-core-practitioner"]
+        )
+        npi_identifier = Identifier(
+            system="http://terminology.hl7.org/NamingSystem/npi",
+            value=str(obj.npi.npi),
+            type=CodeableConcept(
+                coding=[Coding(
+                    system="http://terminology.hl7.org/CodeSystem/v2-0203",
+                    code="PRN",
+                    display="Provider number"
+                )]
+            ),
+            use='official',
+            period=Period(
+                start=obj.npi.enumeration_date,
+                end=obj.npi.deactivation_date
+            )
+        )
+        practitioner.telecom = representation['individual']['telecom']
+        practitioner.identifier = [npi_identifier]
+        if 'identifier' in representation.keys():
+            practitioner.identifier += representation['identifier']
+        practitioner.name = representation['individual']['name']
+        if 'taxonomy' in representation.keys():
+            practitioner.qualification = representation['taxonomy']
+        return practitioner.model_dump()
+
 
 class FHIRSerializer(serializers.Serializer):
     """
     Base serializer for FHIR resources
     """
+
     def to_representation(self, instance):
         """
         Convert FHIR resource to JSON
@@ -95,12 +271,14 @@ class FHIRSerializer(serializers.Serializer):
             return instance.dict()
         return super().to_representation(instance)
 
+
 class PractitionerFHIRSerializer(FHIRSerializer):
     """
     Serializer for FHIR Practitioner resource
     """
     class Meta:
         model = Practitioner
+
 
 class BundleSerializer(FHIRSerializer):
     """
@@ -113,33 +291,33 @@ class BundleSerializer(FHIRSerializer):
 def create_bundle(resources, bundle_type="searchset"):
     """
     Create a FHIR Bundle containing multiple resources
-    
+
     Args:
         resources: List of FHIR resources to include in the bundle
         bundle_type: Type of bundle (e.g. 'searchset', 'collection')
-    
+
     Returns:
         A FHIR Bundle resource
     """
     entries = []
-    
+
     for resource in resources:
         # Get the resource type (Patient, Practitioner, etc.)
         resource_type = resource.get_resource_type()
-        
+
         # Create an entry for this resource
         entry = {
             "fullUrl": f"{resource_type}/{resource.id}",
             "resource": resource,
         }
-        
+
         entries.append(entry)
-    
+
     # Create the bundle
     bundle = Bundle(
         type=bundle_type,
         entry=entries,
         total=len(entries)
     )
-    
+
     return bundle
--- src/ndhfhir/views.py
+++ src/ndhfhir/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.postgres.search import SearchVector
 from django.db.models import Prefetch
-#from djangp.db.models import FilteredRelation, Q
+# from djangp.db.models import FilteredRelation, Q
 from rest_framework import viewsets, generics
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
@@ -13,9 +13,9 @@ from .serializers import PractitionerSerializer, PractitionerFHIRSerializer, Bun
 from .mappings import genderMapping
 
 
-
 def index(request):
     return HttpResponse("Connection to ndh database: successful")
+
 
 def health(request):
     return HttpResponse("healthy")
@@ -25,9 +25,9 @@ class FHIRPractitionerViewSet(viewsets.ViewSet):
     """
     ViewSet for FHIR Practitioner resources
     """
-    #permission_classes = [permissions.IsAuthenticated]
-    def list(self, request):
+    # permission_classes = [permissions.IsAuthenticated]
 
+    def list(self, request):
         """
         Return a list of all providers as FHIR Practitioner resources
         parameters:
@@ -46,7 +46,9 @@ class FHIRPractitionerViewSet(viewsets.ViewSet):
 
         all_params = request.query_params
 
-        providers = Provider.objects.all().prefetch_related('providertootheridentifier_set')#.prefetch_related('individual', 'providertonucctaxonomycode_set', 'providertootheridentifier_set').all() #, 'providertootheridentifier__otheridentifiertype_set'
+        # .prefetch_related('individual', 'providertonucctaxonomycode_set', 'providertootheridentifier_set').all() #, 'providertootheridentifier__otheridentifiertype_set'
+        providers = Provider.objects.all().prefetch_related(
+            'npi', 'individual', 'individual__individualtoname_set', 'providertootheridentifier_set', 'providertonucctaxonomycode_set')
 
         for param, value in all_params.items():
             if param == 'page_size':
@@ -58,55 +60,56 @@ class FHIRPractitionerViewSet(viewsets.ViewSet):
                     page_size = page_size
             if param == 'name':
                 providers = providers.annotate(
-                    search = SearchVector('individual__individualtoname__last_name','individual__individualtoname__first_name','individual__individualtoname__middle_name')
-                ).filter(search = value)
+                    search=SearchVector('individual__individualtoname__last_name',
+                                        'individual__individualtoname__first_name', 'individual__individualtoname__middle_name')
+                ).filter(search=value)
             if param == 'gender':
                 gender = genderMapping.toNDH(value)
-                providers = providers.filter(individual__gender_code = gender)
+                providers = providers.filter(individual__gender_code=gender)
             if param == 'practitioner_type':
                 providers = providers.annotate(
-                    search = SearchVector('individual__providertonucctaxonomycode__nucc_taxonomy_code__display_name')
-                ).filter(search = value)
-            #if param == 'address-state':
+                    search=SearchVector(
+                        'individual__providertonucctaxonomycode__nucc_taxonomy_code__display_name')
+                ).filter(search=value)
+            # if param == 'address-state':
             #    providers = providers.filter(individual__individualtoaddress__address__addressus__fipsstate__abbreviation = value) #fipsstate__abbreviation
-        
 
         paginator = PageNumberPagination()
         paginator.page_size = page_size
         queryset = paginator.paginate_queryset(providers, request)
 
         # Convert each provider to a FHIR Practitioner
-        #fhir_practitioners = [create_fhir_practitioner(provider) for provider in queryset]
-        
+        # fhir_practitioners = [create_fhir_practitioner(provider) for provider in queryset]
+
         # Create a Bundle containing all practitioners
-        #bundle = create_bundle(fhir_practitioners)
-        
+        # bundle = create_bundle(fhir_practitioners)
+
         # Serialize the bundle
-        #serializer = BundleSerializer(bundle)
+        # serializer = BundleSerializer(bundle)
         serializer = PractitionerSerializer(queryset, many=True)
-        
+
         # Set appropriate content type for FHIR responses
         response = paginator.get_paginated_response(serializer.data)
         response["Content-Type"] = "application/fhir+json"
-        
+
         return response
-    
+
     def retrieve(self, request, pk=None):
         """
         Return a single provider as a FHIR Practitioner resource
         """
         provider = get_object_or_404(Provider, pk=int(pk))
 
-        #serializer = PractitionerSerializer(provider)
-        
+        # serializer = PractitionerSerializer(provider)
+
         # Convert provider to FHIR Practitioner
         fhir_practitioner = create_fhir_practitioner(provider)
-        
+
         # Serialize the FHIR Practitioner
         serializer = PractitionerFHIRSerializer(fhir_practitioner)
-        
+
         # Set appropriate content type for FHIR responses
         response = Response(serializer.data)
         response["Content-Type"] = "application/fhir+json"
-        
+
         return response
