57beb320eeea3ede04d3b5c7aebd83c9033b522d
PR0I <sarah.popelka1@cms.hhs.gov>
2025-07-30 09:52:18 -0400

    caching nucc_taxonomy_code

--- src/ndhfhir/cache.py
+++ src/ndhfhir/cache.py
@@ -1,6 +1,7 @@
 from django.core.cache import cache
 import json
-from .models import OtherIdentifierType, FhirNameUse
+from .models import OtherIdentifierType, FhirNameUse, NuccTaxonomyCode
+
 
 def cacheData(model):
     name = model.__name__
@@ -8,12 +9,17 @@ def cacheData(model):
     if not data:
         data = {}
         for obj in model.objects.all():
-            data[obj.id] = obj.value
+            if hasattr(obj, 'display_name'):
+                data[obj.id] = obj.display_name
+            else:
+                data[obj.id] = obj.value
         json_data = json.dumps(data)
         cache.set(name, json_data, timeout=60 * 5)
     else:
-        data=json.loads(data)
-    return data 
+        data = json.loads(data)
+    return data
+
 
 other_identifier_type = cacheData(OtherIdentifierType)
 fhir_name_use = cacheData(FhirNameUse)
+nucc_taxonomy_codes = cacheData(NuccTaxonomyCode)
