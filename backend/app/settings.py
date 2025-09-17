619c64077cc907028dc46169f8c1a0b1700fa92b
PR0I <sarah.popelka1@cms.hhs.gov>
2025-09-17 15:37:34 -0400

    fixing styling

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -41,6 +41,7 @@ INTERNAL_APIS = config("DJANGO_ALLOWED_HOSTS").split(',')
 # Application definition
 
 INSTALLED_APPS = [
+    'npdfhir.apps.NPDFHIRConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -51,7 +52,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'drf_yasg',
-    'npdfhir.apps.NPDFHIRConfig'
 ]
 
 if not TESTING:
