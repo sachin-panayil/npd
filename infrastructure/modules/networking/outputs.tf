2c8b3aaba8ce26145a710668be138b0707a53add
Blaine Price <william.price@cms.hhs.gov>
2025-10-23 09:56:37 -0400

    correct security group

--- infrastructure/modules/networking/outputs.tf
+++ infrastructure/modules/networking/outputs.tf
@@ -1,6 +1,6 @@
 output "db_security_group_id" {
   description = "A list of security group IDs for use with the databases"
-  value       = aws_security_group.fhir_api_db_sg.id
+  value       = aws_security_group.fhir_etl_db_sg.id
 }
 
 output "db_subnet_group_name" {
